eb9016df7dc98447effb29cbe48bf82bb17028a1
Jeff Arnold <jeffrey.arnold@gmail.com>
2011-11-16 10:39:33 -0500

    loads data from Intra- and Non-State wars into war4_partic_dates

--- irdata/load/war4.py
+++ irdata/load/war4.py
@@ -1,8 +1,7 @@
 """ Load data from COW Inter-, Intra-, Non-State wars into the database """
 ## This code is pretty ugly because the data are not very clean, and organized in
 ## a non-relational way
-# TODO: add participant dates for Intra-, and Non-State wars
-# TODO: add the relationships between wars
+# TODO: add the links between wars
 import collections
 import datetime
 import zipfile
@@ -30,6 +29,10 @@ def belligerent_key(ccode, name):
     :param name: str. belligerent name
     :rtype: str. belligerent key
     """
+    try:
+        ccode = int(ccode) if int(ccode) > 0 else None
+    except TypeError:
+        ccode = None
     return u"%s %s" % (ccode, name)
 
 def load_war4(src):
@@ -52,18 +55,18 @@ def load_war4(src):
     def partic_dates(row, n):
         y = model.War4ParticDate()
         y.war_num = row['war_num']
-        ccode = int(row['ccode'])
-        row['ccode'] = ccode if ccode > 0 else None
         y.belligerent = belligerent_key(row['ccode'], row['state_name'])
         y.side = int(row['side']) == 2
         y.partic_num = n
         y.start_year = row['start_year%d' % n]
         y.start_month = row['start_month%d' % n]
         y.start_day = row['start_day%d' % n]
-        if row['start_day%d' % n] == "-7":
+        if row['end_day%d' % n] == "-7":
             y.end_year = model.War4.ONGOING_DATE.year
+            y.ongoing = True
         else:
             y.end_year = row['end_year%d' % n]
+            y.ongoing = False
         if row['end_month%d' % n] == "-7":
             y.end_month = model.War4.ONGOING_DATE.month
         else:
@@ -125,16 +128,15 @@ def load_war4_intra(src):
                 obj = model.War4Belligerent(
                     belligerent = belligerent,
                     belligerent_name = name,
-                    ccode = ccode)
+                    ccode = _int(ccode))
                 session.add(obj)
             session.flush()
             
-    def partic(row, side):
+    def partic(row, belligerent, side):
         y = model.War4Partic()
         y.war_num = _int(row['war_num'])
-        y.belligerent = belligerent_key(_int(row['ccode_%s' % side]),
-                                        unicode(row['side_%s' % side]))
-        y.side = side == "b"
+        y.belligerent = belligerent
+        y.side = (side == 'b')
         y.where_fought = row['where_fought']
         ## outcomes given in Side A / Side B rather than winner/loser
         ## per participant
@@ -145,6 +147,31 @@ def load_war4_intra(src):
         y.outcome = outcome
         y.bat_death = _int(row['side_%sdeaths' % side])
         y.initiator = (row['initiator'] == y.belligerent)
+        return y
+
+    def partic_dates(row, belligerent, side, n):
+        y = model.War4ParticDate()
+        y.war_num = row['war_num']
+        y.belligerent = belligerent
+        y.side = (side == 'b')
+        y.partic_num = n
+        y.start_year = _int(row['start_year%d' % n])
+        y.start_month = _int(row['start_month%d' % n])
+        y.start_day = _int(row['start_day%d' % n])
+        if row['end_day%d' % n] == "-7":
+            y.end_year = model.War4.ONGOING_DATE.year
+            y.ongoing = True
+        else:
+            y.end_year = _int(row['end_year%d' % n])
+            y.ongoing = False
+        if row['end_month%d' % n] == "-7":
+            y.end_month = model.War4.ONGOING_DATE.month
+        else:
+            y.end_month = _int(row['end_month%d' % n])
+        if row['end_month%d' % n] == "-7":        
+            y.end_day = model.War4.ONGOING_DATE.day
+        else:
+            y.end_day = _int(row['end_day%d' % n])
         return y
     
     session = model.SESSION()
@@ -163,14 +190,21 @@ def load_war4_intra(src):
             for side in (False, True):
                 session.add(model.War4Side(side=side, war_num=row['war_num']))
             session.flush()
-        add_belligerent(session, row['side_a'], _int(row['ccode_a']))
-        add_belligerent(session, row['side_b'], _int(row['ccode_b']))
         if _side(row['side_a']):
-            session.add(partic(row, 'a'))
+            add_belligerent(session, row['side_a'], row['ccode_a'])
+            belligerent = belligerent_key(row['ccode_a'], row['side_a'])
+            session.add(partic(row, belligerent, 'a'))
+            session.add(partic_dates(row, belligerent, 'a', 1))
+            if row['start_year2'] != '-8':
+                session.add(partic_dates(row, belligerent, 'a', 2))
         if _side(row['side_b']):
-            session.add(partic(row, 'b'))
+            add_belligerent(session, row['side_b'], row['ccode_b'])
+            belligerent = belligerent_key(row['ccode_b'], row['side_b'])
+            session.add(partic(row, belligerent, 'b'))
+            session.add(partic_dates(row, belligerent, 'b', 1))
+            if row['start_year2'] != '-8':
+                session.add(partic_dates(row, belligerent, 'b', 2))
         session.flush()
-        
     session.commit()
 
 def load_war4_nonstate(src):
@@ -193,7 +227,8 @@ def load_war4_nonstate(src):
             if q.count() == 0:
                 obj = model.War4Belligerent(
                     belligerent = belligerent,
-                    belligerent_name = name)
+                    belligerent_name = name,
+                    ccode = ccode)
                 session.add(obj)
             session.flush()
             
@@ -211,6 +246,31 @@ def load_war4_nonstate(src):
         y.initiator = (row['initiator'] == side.upper())
         return y
 
+    def partic_dates(row, name, side):
+        y = model.War4ParticDate()
+        y.war_num = row['war_num']
+        y.belligerent = belligerent_key(None, name)
+        y.side = side
+        y.partic_num = 1
+        y.start_year = _int(row['start_year'])
+        y.start_month = _int(row['start_month'])
+        y.start_day = _int(row['start_day'])
+        if row['end_year'] == "-7":
+            y.end_year = model.War4.ONGOING_DATE.year
+            y.ongoing = True
+        else:
+            y.end_year = _int(row['end_year'])
+            y.ongoing = False
+        if row['end_month'] == "-7":
+            y.end_month = model.War4.ONGOING_DATE.month
+        else:
+            y.end_month = _int(row['end_month'])
+        if row['end_month'] == "-7":        
+            y.end_day = model.War4.ONGOING_DATE.day
+        else:
+            y.end_day = _int(row['end_day'])
+        return y
+
     session = model.SESSION()
     reader = csv2.DictReader(src, encoding='latin1')
     reader.fieldnames = [utils.camel2under(x) for x in reader.fieldnames]
@@ -224,18 +284,20 @@ def load_war4_nonstate(src):
         for side in ('a', 'b'):
             session.add(model.War4Side(side=(side == 'b'),
                                        war_num=row['war_num'],
-                                       bat_death = row['side_%sdeaths' % side]))
+                                       bat_death = _int(row['side_%sdeaths' % side])))
         session.flush()
         for i in (1, 2):
             name = row['side_a%d' % i]
             if name != '-8':
                 add_belligerent(session, name)
                 session.add(partic(row, 'a', name))
+                session.add(partic_dates(row, name, False))
         for i in range(1, 6):
             name = row['side_b%d' % i]
             if name != '-8':
                 add_belligerent(session, name)
                 session.add(partic(row, 'b', name))
+                session.add(partic_dates(row, name, True))
         session.flush()
     session.commit()
 
